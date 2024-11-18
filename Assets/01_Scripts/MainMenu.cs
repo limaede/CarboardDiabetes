7c30c4204849ab92d58301de24b60edcdbef8351
NicoleHerbas <hfn5001384@est.univalle.edu>
2024-11-18 05:08:47 -0400

    Configuracion de botones del menu para el mando

--- Assets/01_Scripts/MainMenu.cs
+++ Assets/01_Scripts/MainMenu.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Awake()
+    {
+        inputSystem = new NewInputSystem();
+        inputSystem.Player.Shoot.performed += ctx => PlayGame();
+        inputSystem.Player.Charge.performed += ctx => QuitGame();
+
+
+    }
+
+    private NewInputSystem inputSystem;
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
